88a6f0519e6c148d7fce04421d2992c0496ff07c
Shahar Yakir <shahar.yakir@gmail.com>
2023-08-10 17:21:35 +0100

    separate recover functionality

--- test/rewards.test.ts
+++ test/rewards.test.ts
@@ -1,6 +1,6 @@
 import { block, bn18 } from "@defi.org/web3-candies";
 import { expect } from "chai";
-import { locking, xctd, rewardToken, user1, user2, withFixture, fundWithXCTD, deployer, advanceMonths, INITIAL_MONTH } from "./fixture";
+import { locking, xctd, rewardToken, user1, user2, withFixture, fundWithXCTD, deployer, advanceMonths, INITIAL_MONTH, rewards } from "./fixture";
 import BN from "bignumber.js";
 import { expectRevert } from "@defi.org/web3-candies/dist/hardhat";
 
@@ -11,9 +11,9 @@ describe("Rewards", () => {
     const amount = 1234.567891234567;
     beforeEach(async () => fundWithXCTD(amount));
     beforeEach(async () => {
-      await rewardToken.methods.approve(locking.options.address, await rewardToken.amount(50_000)).send({ from: deployer });
+      await rewardToken.methods.approve(rewards.options.address, await rewardToken.amount(50_000)).send({ from: deployer });
       // 50000 / (60 * 86400) = 0.00964506
-      await locking.methods.addReward(rewardToken.options.address, 0, 5, (await rewardToken.amount(10_000)).toFixed(0)).send({ from: deployer });
+      await rewards.methods.addReward(rewardToken.options.address, 0, 5, (await rewardToken.amount(10_000)).toFixed(0)).send({ from: deployer });
     });
 
     it("user should have reward if did stake and didn't claim so far", async () => {
@@ -26,7 +26,7 @@ describe("Rewards", () => {
 
       await locking.methods.lock(await xctd.amount(amount), 3).send({ from: user1 });
       await advanceMonths(1);
-      const pendingRewards = await locking.methods.pendingRewards(user1, rewardToken.options.address).call();
+      const pendingRewards = await rewards.methods.pendingRewards(user1, rewardToken.options.address).call();
       expect(pendingRewards).bignumber.closeTo(bn18(10_000), bn18(1));
     });
 
@@ -36,8 +36,8 @@ describe("Rewards", () => {
 
       await advanceMonths(1);
 
-      const userOnePendingRewards = await locking.methods.pendingRewards(user1, rewardToken.options.address).call();
-      const userTwoPendingRewards = await locking.methods.pendingRewards(user2, rewardToken.options.address).call();
+      const userOnePendingRewards = await rewards.methods.pendingRewards(user1, rewardToken.options.address).call();
+      const userTwoPendingRewards = await rewards.methods.pendingRewards(user2, rewardToken.options.address).call();
 
       expect(userOnePendingRewards).bignumber.closeTo(bn18(5_000), 1e18);
       expect(userTwoPendingRewards).bignumber.closeTo(bn18(5_000), 1e18);
@@ -49,8 +49,8 @@ describe("Rewards", () => {
 
       await advanceMonths(1);
 
-      const userOnePendingRewards = await locking.methods.pendingRewards(user1, rewardToken.options.address).call();
-      const userTwoPendingRewards = await locking.methods.pendingRewards(user2, rewardToken.options.address).call();
+      const userOnePendingRewards = await rewards.methods.pendingRewards(user1, rewardToken.options.address).call();
+      const userTwoPendingRewards = await rewards.methods.pendingRewards(user2, rewardToken.options.address).call();
 
       expect(userOnePendingRewards).bignumber.closeTo(bn18(6_666), 1e18);
       expect(userTwoPendingRewards).bignumber.closeTo(bn18(3_333), 1e18);
@@ -62,8 +62,8 @@ describe("Rewards", () => {
 
       await advanceMonths(1);
 
-      const userOnePendingRewards = await locking.methods.pendingRewards(user1, rewardToken.options.address).call();
-      const userTwoPendingRewards = await locking.methods.pendingRewards(user2, rewardToken.options.address).call();
+      const userOnePendingRewards = await rewards.methods.pendingRewards(user1, rewardToken.options.address).call();
+      const userTwoPendingRewards = await rewards.methods.pendingRewards(user2, rewardToken.options.address).call();
 
       expect(userOnePendingRewards).bignumber.closeTo(bn18(6_966), 1e18);
       expect(userTwoPendingRewards).bignumber.closeTo(bn18(3_034), 1e18);
@@ -75,8 +75,8 @@ describe("Rewards", () => {
 
       await advanceMonths(1);
 
-      const userOnePendingRewards = await locking.methods.pendingRewards(user1, rewardToken.options.address).call();
-      const userTwoPendingRewards = await locking.methods.pendingRewards(user2, rewardToken.options.address).call();
+      const userOnePendingRewards = await rewards.methods.pendingRewards(user1, rewardToken.options.address).call();
+      const userTwoPendingRewards = await rewards.methods.pendingRewards(user2, rewardToken.options.address).call();
 
       expect(userOnePendingRewards).bignumber.closeTo(bn18(8_406), 1e18);
       expect(userTwoPendingRewards).bignumber.closeTo(bn18(1_594), 1e18);
@@ -86,7 +86,7 @@ describe("Rewards", () => {
       await advanceMonths(1);
       await locking.methods.lock(await xctd.amount(amount), 3).send({ from: user1 });
       await advanceMonths(1);
-      const pendingRewards = await locking.methods.pendingRewards(user1, rewardToken.options.address).call();
+      const pendingRewards = await rewards.methods.pendingRewards(user1, rewardToken.options.address).call();
       expect(pendingRewards).bignumber.closeTo(bn18(10_000), 1e18);
     });
 
@@ -94,34 +94,34 @@ describe("Rewards", () => {
       await advanceMonths(1);
       await locking.methods.lock(await xctd.amount(amount), 3).send({ from: user1 });
       await advanceMonths(4);
-      const pendingRewards = await locking.methods.pendingRewards(user1, rewardToken.options.address).call();
+      const pendingRewards = await rewards.methods.pendingRewards(user1, rewardToken.options.address).call();
       expect(pendingRewards).bignumber.closeTo(bn18(30_000), 1e18);
       // note that 10K got stuck at the beginning of the program, and another 10K got stuck at the end
-      const rewardTokenBalance = BN(await rewardToken.methods.balanceOf(locking.options.address).call());
+      const rewardTokenBalance = BN(await rewardToken.methods.balanceOf(rewards.options.address).call());
       expect(rewardTokenBalance.minus(pendingRewards)).bignumber.closeTo(bn18(20_000), 1e18);
 
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
-      expect(await rewardToken.methods.balanceOf(locking.options.address).call()).bignumber.closeTo(bn18(20_000), 1e18);
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      expect(await rewardToken.methods.balanceOf(rewards.options.address).call()).bignumber.closeTo(bn18(20_000), 1e18);
     });
 
     it("one user locks for three months, claims after 1 month", async () => {
       await locking.methods.lock(await xctd.amount(amount), 3).send({ from: user1 });
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.zero;
       await advanceMonths(1);
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.closeTo(bn18(10_000), 1e18);
     });
 
     it("one user locks for three months, claims after 1 month, shouldn't be able to claim again", async () => {
       await locking.methods.lock(await xctd.amount(amount), 3).send({ from: user1 });
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.zero;
       await advanceMonths(1);
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.closeTo(bn18(10_000), 1e18);
 
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.closeTo(bn18(10_000), 1e18);
     });
 
@@ -130,10 +130,10 @@ describe("Rewards", () => {
       await advanceMonths(1);
       await locking.methods.lock(await xctd.amount(amount), 4).send({ from: user2 });
       await advanceMonths(4);
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
 
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.closeTo(bn18(15_142), 1e18);
-      await locking.methods.claim(user2, rewardToken.options.address).send({ from: user2 });
+      await rewards.methods.claim(user2, rewardToken.options.address).send({ from: user2 });
       expect(await rewardToken.methods.balanceOf(user2).call()).bignumber.closeTo(bn18(34_858), 1e18);
     });
 
@@ -141,7 +141,7 @@ describe("Rewards", () => {
       await locking.methods.lock(await xctd.amount(amount), 2).send({ from: user1 });
       await advanceMonths(1);
       await locking.methods.lock(await xctd.amount(amount), 2).send({ from: user2 });
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.closeTo(bn18(10_000), 1e18);
     });
 
@@ -151,38 +151,38 @@ describe("Rewards", () => {
       await locking.methods.lock(amountToLock, 24).send({ from: user2 });
       await locking.methods.earlyWithdrawWithPenalty(amountToLock).send({ from: user2 });
       await advanceMonths(1);
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.closeTo(bn18(10_000), 1e18);
     });
 
     it("rewards program is updateable, same period", async () => {
-      await rewardToken.methods.approve(locking.options.address, await rewardToken.amount(15_000)).send({ from: deployer });
-      await locking.methods.addReward(rewardToken.options.address, 0, 5, (await rewardToken.amount(3_000)).toFixed(0)).send({ from: deployer });
+      await rewardToken.methods.approve(rewards.options.address, await rewardToken.amount(15_000)).send({ from: deployer });
+      await rewards.methods.addReward(rewardToken.options.address, 0, 5, (await rewardToken.amount(3_000)).toFixed(0)).send({ from: deployer });
       await locking.methods.lock(await xctd.amount(amount), 5).send({ from: user1 });
       await advanceMonths(5);
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.closeTo(bn18(65_000), 1e18);
     });
 
     it("rewards program is updateable, different period", async () => {
       await advanceMonths(10);
-      await rewardToken.methods.approve(locking.options.address, await rewardToken.amount(15_000)).send({ from: deployer });
-      await locking.methods.addReward(rewardToken.options.address, 0, 5, (await rewardToken.amount(3_000)).toFixed(0)).send({ from: deployer });
+      await rewardToken.methods.approve(rewards.options.address, await rewardToken.amount(15_000)).send({ from: deployer });
+      await rewards.methods.addReward(rewardToken.options.address, 0, 5, (await rewardToken.amount(3_000)).toFixed(0)).send({ from: deployer });
 
       await locking.methods.lock(await xctd.amount(amount), 5).send({ from: user1 });
       await advanceMonths(5);
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.closeTo(bn18(15_000), 1e18);
     });
 
     it("rewards program is updateable, offset", async () => {
-      await rewardToken.methods.approve(locking.options.address, await rewardToken.amount(15_000)).send({ from: deployer });
-      await locking.methods.addReward(rewardToken.options.address, 10, 5, (await rewardToken.amount(3_000)).toFixed(0)).send({ from: deployer });
+      await rewardToken.methods.approve(rewards.options.address, await rewardToken.amount(15_000)).send({ from: deployer });
+      await rewards.methods.addReward(rewardToken.options.address, 10, 5, (await rewardToken.amount(3_000)).toFixed(0)).send({ from: deployer });
 
       await advanceMonths(10);
       await locking.methods.lock(await xctd.amount(amount), 5).send({ from: user1 });
       await advanceMonths(5);
-      await locking.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
+      await rewards.methods.claim(user1, rewardToken.options.address).send({ from: user1 });
       expect(await rewardToken.methods.balanceOf(user1).call()).bignumber.closeTo(bn18(15_000), 1e18);
     });
 
@@ -197,18 +197,18 @@ describe("Rewards", () => {
     it("owner can only recover reward funds per month a single time", async () => {
       const initialBalance = await rewardToken.methods.balanceOf(deployer).call();
       await advanceMonths(6);
-      await locking.methods.recover(rewardToken.options.address, INITIAL_MONTH, INITIAL_MONTH + 5).send({ from: deployer });
+      await rewards.methods.recover(rewardToken.options.address, INITIAL_MONTH, INITIAL_MONTH + 5).send({ from: deployer });
       expect(await rewardToken.methods.balanceOf(deployer).call()).bignumber.eq(BN(initialBalance).plus(await rewardToken.amount(50_000)));
-      await locking.methods.recover(rewardToken.options.address, INITIAL_MONTH, INITIAL_MONTH + 5).send({ from: deployer });
+      await rewards.methods.recover(rewardToken.options.address, INITIAL_MONTH, INITIAL_MONTH + 5).send({ from: deployer });
       expect(await rewardToken.methods.balanceOf(deployer).call()).bignumber.eq(BN(initialBalance).plus(await rewardToken.amount(50_000)));
     });
 
     it("recover reward tokens that don't belong to the reward program", async () => {
       await locking.methods.lock(await xctd.amount(amount), 24).send({ from: user1 });
-      await rewardToken.methods.transfer(locking.options.address, await rewardToken.amount(17_000)).send({ from: deployer });
+      await rewardToken.methods.transfer(rewards.options.address, await rewardToken.amount(17_000)).send({ from: deployer });
       const balanceBefore = await rewardToken.methods.balanceOf(deployer).call();
       await advanceMonths(1);
-      await locking.methods.recover(rewardToken.options.address, 0, 0).send({ from: deployer });
+      await rewards.methods.recover(rewardToken.options.address, 0, 0).send({ from: deployer });
       const balanceAfter = await rewardToken.methods.balanceOf(deployer).call();
       expect(BN(balanceAfter).minus(balanceBefore)).bignumber.eq(await rewardToken.amount(17_000));
     });
